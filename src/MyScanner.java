1d1491c6ad590946bfc0e8df3795875e151c0987
criocat <razinkov403@gmail.com>
2023-10-19 19:02:01 +0300

    fiks 1

--- src/MyScanner.java
+++ src/MyScanner.java
@@ -6,72 +6,75 @@ import java.lang.Math;
 public class MyScanner {
     private Reader rd;
     private final int buffSize = 1024;
-    private String str = "";
+    private final String lineSeparator = System.lineSeparator();
     private int pos = buffSize;
-    private final char nullChar = (char)(-1);
+    private final char nullChar = (char) -1;
     private char[] buffer = new char[buffSize];
 
     public MyScanner(InputStream in) {
         rd = new InputStreamReader(in);
-        str = new String(buffer);
     }
 
     public MyScanner(String path, Charset charset) {
         try {
             rd = new FileReader(path, charset);
-            str = new String(buffer);
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.print("the error occurred when opening the file " + e.getMessage());
         }
     }
 
-    public boolean good(char c) {
-        return Character.isAlphabetic(c) || c == '\'' || Character.getType(c) == Character.DASH_PUNCTUATION;
+    private boolean good(char c, boolean readDigits) {
+        boolean res = Character.isAlphabetic(c) || c == '\'' || Character.getType(c) == Character.DASH_PUNCTUATION;
+        return (readDigits ? res || Character.isDigit(c) : res);
     }
 
     private void updateBuffer() {
         try {
-            int cnt = Math.max(rd.read(buffer, 0, buffSize), 0);
-            if (cnt != buffSize) {
-                buffer[cnt] = nullChar;
+            int readedLen = 0;
+            while (readedLen != buffSize) {
+                int cnt = rd.read(buffer, readedLen, buffSize - readedLen);
+                if (cnt == -1) {
+                    buffer[readedLen] = nullChar;
+                    break;
+                }
+                readedLen += cnt;
             }
-            str = new String(buffer);
         } catch (IOException e) {
             System.out.print("reading error " + e.getMessage());
         }
     }
 
-    public String next() {
+    public String next(boolean readDigits) {
         while (true) {
-            if (pos == str.length()) {
+            if (pos == buffSize) {
                 updateBuffer();
                 pos = 0;
             }
-            if (str.charAt(pos) == nullChar || good(str.charAt(pos))) {
+            if (buffer[pos] == nullChar || good(buffer[pos], readDigits)) {
                 break;
             }
             ++pos;
         }
-        if (pos != str.length() && buffer[pos] == nullChar) {
+        if (pos != buffSize && buffer[pos] == nullChar) {
             return null;
         }
         int pointerR;
         StringBuilder res = new StringBuilder();
         boolean first = true;
-        while (pos == str.length() || first) {
+        while (pos == buffSize || first) {
             first = false;
-            if (pos == str.length()) {
+            if (pos == buffSize) {
                 pos = 0;
                 updateBuffer();
             }
             pointerR = pos;
-            for (int i = pos; i < str.length(); ++i) {
+            for (int i = pos; i < buffSize; ++i) {
                 pointerR = i;
-                if (!good(str.charAt(i))) {
+                if (!good(buffer[i], readDigits)) {
                     --pointerR;
                     break;
                 }
-                res.append(str.charAt(i));
+                res.append(buffer[i]);
             }
             pos = pointerR + 1;
         }
@@ -81,7 +84,7 @@ public class MyScanner {
     public String nextLine() {
         StringBuilder res = new StringBuilder();
         while (true) {
-            if (pos == str.length()) {
+            if (pos == buffSize) {
                 updateBuffer();
                 pos = 0;
             }
@@ -91,23 +94,29 @@ public class MyScanner {
                 }
                 return res.toString();
             }
-            res.append(str.charAt(pos));
+            res.append(buffer[pos]);
             ++pos;
-            if (res.length() >= System.lineSeparator().length() && res.substring(res.length() - System.lineSeparator().length()).equals(System.lineSeparator())) {
-                return res.substring(0, res.length() - System.lineSeparator().length());
+            if (res.length() >= lineSeparator.length() && res.substring(res.length() - lineSeparator.length()).equals(lineSeparator)) {
+                return res.substring(0, res.length() - lineSeparator.length());
             }
+        }
+    }
+
+    public boolean hasLineSeparator(boolean readDigits) {
+        while (true) {
+            if (pos == buffSize) {
+                updateBuffer();
+                pos = 0;
+            }
+
         }
     }
 
     public void close() {
         try {
-        rd.close();
-        } catch(IOException e) {
+            rd.close();
+        } catch (IOException e) {
             System.out.print("error when trying to close the file " + e.getMessage());
         }
-    }
-
-    public char getNullChar() {
-        return nullChar;
     }
 }
